7436c6d0bd29ce2180fb0823e5d4fd0b3415fa3b
Igor <igorshk271@gmail.com>
2026-01-24 15:20:37 +0200

    test

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/api/hello', (req, res) => {
-    res.json({ message: 'Hello from backend 👋' });
+    res.json({ message: 'Hello from backend' });
 });
 
 const PORT = 5000;
